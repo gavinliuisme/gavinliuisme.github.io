c99e12c061cb0687d527b8c8909e4f726123dc1c
星峰 <34774269+gavinliuisme@users.noreply.github.com>
2024-06-29 00:21:53 +0800

    Update main.js

--- files/main.js
+++ files/main.js
@@ -1,3 +1,4 @@
 const alist = document.querySelectorAll('a');
 alist[alist.length-2].href = 'https://github.com/gavinliuisme/gavinliuisme.github.io';
 alist[alist.length-2].target="_blank";
+alert(1);
