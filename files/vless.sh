08052a27b88693ed90f98a0f28c7a1b44706dbb0
星峰 <34774269+gavinliuisme@users.noreply.github.com>
2024-07-07 12:58:20 +0800

    Update vless.sh

--- files/vless.sh
+++ files/vless.sh
@@ -5,20 +5,27 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+echo '安装pm2'
 bash <(curl -s https://raw.githubusercontent.com/k0baya/alist_repl/main/serv00/install-pm2.sh)
 
 cd ~/domains/$USER.serv00.net
 
+echo '安装vless'
 git clone https://github.com/qwer-search/serv00-vless && mv -f serv00-vless vless && cd vless && rm -f README.md
 
+echo '安装依赖'
 npm install
 
+echo '替换端口'
 sed -i '' "s/|| 3000/|| $1/" app.js
 
 if [ -n "$2" ]; then
+    echo '替换uuid'
     sed -i '' "s/37a0bd7c-8b9f-4693-8916-bd1e2da0a817/$2/" app.js
 fi
 
+echo '启动vless'
 ~/.npm-global/bin/pm2 start app.js --name vless
 
+echo '保存pm进程'
 ~/.npm-global/bin/pm2 save
