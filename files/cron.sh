18a60eac1c1c169e45f77b24005348d50db8dcb3
星峰 <34774269+gavinliuisme@users.noreply.github.com>
2024-07-12 10:02:13 +0800

    Update cron.sh

--- files/cron.sh
+++ files/cron.sh
@@ -1,12 +1,17 @@
 pm2_path=$(which /home/$USER/.npm-global/bin/pm2)
 process_count=$($pm2_path list | grep "vless" | wc -l)
 if [ "$process_count" -gt 1 ]; then
-    echo "The process count for 'vless' is $process_count"
+    process_list=$($pm2_path  jlist | jq -r --arg name "vless" '.[] | select(.name == $name) | .pm_id')
+    first_process_id=$(echo "$process_list" | head -n 1)
+    echo "$process_list" | tail -n +2 | while read -r process_id; do
+        $pm2_path delete "$process_id"
+    done
+    $pm2_path ls
 else
-    status=$(/home/$USER/.npm-global/bin/pm2 info vless | grep "status" | awk '{print $4}')
+    status=$($pm2_path  info vless | grep "status" | awk '{print $4}')
     if [ "$status" == "stopped" ]; then
-        /home/$USER/.npm-global/bin/pm2 restart vless
+        $pm2_path  restart vless
     elif [ "$status" == "" ]; then
-        /home/$USER/.npm-global/bin/pm2 resurrect
+        $pm2_path  resurrect
     fi
 fi
