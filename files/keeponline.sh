8db9a239bf41983c3da8906e4507828cdfde50ef
星峰 <34774269+gavinliuisme@users.noreply.github.com>
2024-07-12 10:21:05 +0800

    Update keeponline.sh

--- files/keeponline.sh
+++ files/keeponline.sh
@@ -7,10 +7,15 @@ if [ "$process_count" -gt 1 ]; then
         $pm2_path delete "$process_id"
     done
     $pm2_path save
+    echo "清理多余的vless进程"
 fi
 status=$($pm2_path  info vless | grep "status" | awk '{print $4}')
 if [ "$status" == "" ]; then
     $pm2_path  resurrect
+    echo "还原vless进程"
 elif [ "$status" != "online" ]; then
     $pm2_path  restart vless
+    echo "重启vless进程"
+else
+    echo "vless进程正常"
 fi
